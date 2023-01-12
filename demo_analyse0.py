06a795b2605a4a4cdd9e7187cd45990f992b8653
axnj2 <52408308+axnj2@users.noreply.github.com>
2023-01-12 16:14:22 +0100

    major refactoring moostly on variable names

--- demo_analyse0.py
+++ demo_analyse0.py
@@ -1,95 +1,109 @@
 import random
 
-AJUSTING_WEIGHTS = {"0": 1.5, "1": 0.75, "2": 0.5, "3": 0.1}
+FACTORS_FOR_WEIGHTS_ADJUSTING = {"0": 1.5, "1": 0.75, "2": 0.5, "3": 0.1}
 MODES = {1: "random", 2: "new"}
-DEMOS_FILE = "demos_analyse0.txt"
-LEGACY_SCORES_FILE = "demo-scores.txt" # to avoid breaking compatibility with older files name
+PROOFS_FILE_LOCATION = "demos_analyse0.txt"
+LEGACY_SCORES_FILE_LOCATION = "demo-scores.txt"  # to avoid breaking compatibility with older files name
 
 
-def initialize_dictionnary():
-    with open(DEMOS_FILE, "r", encoding="utf-8") as f:
-        demos = f.read().splitlines()
-        demo_dict = dict()
-        for number, demo in enumerate(demos, 1):
-            demo_dict[number] = [demo, ]
+def initialize_proofs_and_scores(proofs_file_location):
+    with open(proofs_file_location, "r", encoding="utf-8") as f:
+        proofs_prompt = f.read().splitlines()
+        new_proofs_and_scores = dict()
+        for proof_number, proof in enumerate(proofs_prompt, start=1):
+            new_proofs_and_scores[proof_number] = [proof]
 
-    with open(score_file, "r") as f:
+    with open(scores_file_location, "r") as f:
         scores = map(float, f.read().splitlines())
-        for number, score in enumerate(scores, 1):
-            demo_dict[number].append(score)
+        for proof_number, score in enumerate(scores, 1):
+            new_proofs_and_scores[proof_number].append(score)
 
-    return demo_dict
-
-
-def write_file(data: dict) -> None:
-    with open(score_file, "w") as f:
-        for i in range(1, len(data)):
-            f.write(str(data[i][1]) + "\n")
-        f.write(str(data[len(data)][1]))
+    return new_proofs_and_scores
 
 
-def choose_random_with_weights():
-    probability = [float(dictionnary_of_demo[i][1]) for i in range(1, len(dictionnary_of_demo) + 1)]
-    return random.choices(list(range(1, len(dictionnary_of_demo) + 1)), weights=probability, k=1)[0]
+def write_file(proofs_and_scores: dict) -> None:
+    with open(scores_file_location, "w") as f:
+        for i in range(1, len(proofs_and_scores)):
+            f.write(str(proofs_and_scores[i][1]) + "\n")
+        f.write(str(proofs_and_scores[len(proofs_and_scores)][1]))
 
 
-def choose_random_only_new():
-    new_demos = []
-    for i in range(1, len(dictionnary_of_demo) + 1):
-        if dictionnary_of_demo[i][1] == 1.0:
-            new_demos.append(i)
-    if new_demos:
-        return random.choice(new_demos), len(new_demos)
+def choose_with_weights():
+    probability_distribution = [
+        float(proofs_and_scores[i][1]) for i in range(1, len(proofs_and_scores) + 1)
+    ]
+    return int(random.choices(list(range(1, len(proofs_and_scores) + 1)), weights=probability_distribution, k=1))
+
+
+def choose_only_new():
+    new_proofs = []
+    for i in range(1, len(proofs_and_scores) + 1):
+        if proofs_and_scores[i][1] == 1.0:
+            new_proofs.append(i)
+    if new_proofs:
+        return random.choice(new_proofs), len(new_proofs)
     else:
         return None, None
 
 
-def initialize_modes():
-    user_input = int(input("Quel mode veux-tu ?\n"
-                           "   1 : Au hasard avec des chances d'être choisie ajusté en fonction de la réussite\n"
-                           "   2 : Uniquement les nouvelles (ne fonctionne que si les démos n'ont pas encore toutes été faites)\n"
-                           "input : "))
-    return MODES[user_input]
+def choose_next_proof(current_mode):
+    if MODES[current_mode] == "new":
+        next_proof, number_of_new = choose_only_new()
+        if next_proof is None:
+            print("Tu as fait toutes les démos au moins une fois, mode changé vers random avec le poids.")
+            current_mode = "random"
+        else:
+            print(f"il reste {number_of_new} démonstration(s) pas encore faites")
+    elif MODES[current_mode] == "random":
+        next_proof = choose_with_weights()
+
+    return next_proof, current_mode
 
 
-def initialize_score_file():
+def initialize_score_file(proofs_file_location):
     try:
-        with open(LEGACY_SCORES_FILE, "r") as f:
+        with open(LEGACY_SCORES_FILE_LOCATION, "r"):
             pass
-        score_file = LEGACY_SCORES_FILE
+        score_file = LEGACY_SCORES_FILE_LOCATION
     except FileNotFoundError:
-        score_file = DEMOS_FILE.strip(".txt") + "_scores.txt"
+        score_file = proofs_file_location.strip(".txt") + "_scores.txt"
         try:
-            with open(score_file, "r") as f:
+            with open(score_file, "r"):
                 pass
         except FileNotFoundError:
-            with open(DEMOS_FILE, "r") as f:
+            with open(proofs_file_location, "r") as f:
                 number_of_demo = len(f.readlines())
             with open(score_file, "w") as f:
                 f.write("1.0\n"*(number_of_demo-1) + "1.0")
     return score_file
 
 
-score_file = initialize_score_file()
-mode = initialize_modes()
+def initialize_mode():
+    user_input = int(input(
+        "Quel mode voulez-vous ?\n"
+        "   1 : Au hasard avec des chances d'être choisie ajusté en fonction de la réussite\n"
+        "   2 : Uniquement les nouvelles (ne fonctionne que si les démos n'ont pas encore toutes été faites)\n"
+        "input : "
+    ))
+    if user_input not in MODES:
+        raise Exception(f'"{user_input}" is not one of the supported modes')
+    return user_input
+
+
+scores_file_location = initialize_score_file(PROOFS_FILE_LOCATION)
+mode = initialize_mode()
 studying = True
 while studying:
-    dictionnary_of_demo = initialize_dictionnary()
+    proofs_and_scores = initialize_proofs_and_scores(PROOFS_FILE_LOCATION)
 
-    if mode == "new":
-        todo, number_of_new = choose_random_only_new()
-        if todo is None:
-            print("Tu as fait toutes les démos au moins une fois, mode changé vers random avec le poids.")
-            mode = "random"
-        else:
-            print(f"il reste {number_of_new} démonstration(s) pas encore faites")
-    if mode == "random":
-        todo = choose_random_with_weights()
+    current_proof, mode = choose_next_proof(mode)
 
-    print(dictionnary_of_demo[todo][0])
-    result = input("Démo réussie ? (0=pas du tout, 1=quelques erreure, 2=presque parfait et 3=prêt pour l'examen)\n")
+    print(proofs_and_scores[current_proof][0])
+    success_assessment = input(
+        "Démo réussie ? (0=pas du tout, 1=quelques erreurs, 2=presque parfait et 3=prêt pour l'examen)\n"
+    )
 
-    dictionnary_of_demo[todo][1] *= AJUSTING_WEIGHTS[result]
-    print("nouveau poids :", dictionnary_of_demo[todo][1])
+    proofs_and_scores[current_proof][1] *= FACTORS_FOR_WEIGHTS_ADJUSTING[success_assessment]
+    print("nouveau poids :", proofs_and_scores[current_proof][1])
 
-    write_file(dictionnary_of_demo)
+    write_file(proofs_and_scores)
