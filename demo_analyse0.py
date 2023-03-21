f3c3f37caefa5ebed01572cf624f4a61cc2f95c2
axnj2 <52408308+axnj2@users.noreply.github.com>
2023-03-21 11:53:33 +0100

    Big refactor

--- demo_analyse0.py
+++ demo_analyse0.py
@@ -2,9 +2,27 @@ import random
 
 FACTORS_FOR_WEIGHTS_ADJUSTING = {"0": 1.5, "1": 0.75, "2": 0.5, "3": 0.1}
 MODES = {1: "random", 2: "new", 3: "stats"}
-PROOFS_FILE_LOCATION = "demos_analyse0.txt"
-STATS_FILE_LOCATION = "demos_analyse0_stats.txt"
+PROOFS_FILE_LOCATION = "proof_lists/demos_analyse0.txt"
+STATS_FILE_LOCATION = "proof_lists/demos_analyse0_stats.txt"
 LEGACY_SCORES_FILE_LOCATION = "demo-scores.txt"  # to avoid breaking compatibility with older files name
+
+
+def initialize_score_file(proofs_file_location):
+    try:
+        with open(LEGACY_SCORES_FILE_LOCATION, "r"):
+            pass
+        score_file = LEGACY_SCORES_FILE_LOCATION
+    except FileNotFoundError:
+        score_file = proofs_file_location.strip(".txt") + "_scores.txt"
+        try:
+            with open(score_file, "r"):
+                pass
+        except FileNotFoundError:
+            with open(proofs_file_location, "r") as f:
+                number_of_demo = len(f.readlines())
+            with open(score_file, "w") as f:
+                f.write("1.0\n" * (number_of_demo - 1) + "1.0")
+    return score_file
 
 
 def initialize_proofs_and_scores(proofs_file_location):
@@ -21,9 +39,11 @@ def initialize_proofs_and_scores(proofs_file_location):
 
     return new_proofs_and_scores
 
+
 def create_stats_file():
     with open(STATS_FILE_LOCATION, "w") as f:
-        f.write(" \n"*(len(proofs_and_scores)))
+        f.write(" \n" * (len(proofs_and_scores)))
+
 
 def get_stats():
     try:
@@ -37,11 +57,13 @@ def get_stats():
     stats = [list(map(int, stat.split())) for stat in stats]
     return stats
 
+
 def write_file(proofs_and_scores: dict) -> None:
     with open(scores_file_location, "w") as f:
         for i in range(1, len(proofs_and_scores)):
             f.write(str(proofs_and_scores[i][1]) + "\n")
         f.write(str(proofs_and_scores[len(proofs_and_scores)][1]))
+
 
 def write_stats(proofs_and_scores: dict, demo: int, score: int) -> None:
     try:
@@ -54,7 +76,8 @@ def write_stats(proofs_and_scores: dict, demo: int, score: int) -> None:
     data = file.readlines()
     data[demo - 1] = data[demo - 1].strip() + " " + str(score) + "\n"
     with open(STATS_FILE_LOCATION, 'w') as file:
-        file.writelines( data )
+        file.writelines(data)
+
 
 def choose_with_weights(curent_proofs_and_scores):
     probability_distribution = [
@@ -88,24 +111,6 @@ def choose_next_proof(current_mode, curent_proofs_and_scores):
     return next_proof, current_mode
 
 
-def initialize_score_file(proofs_file_location):
-    try:
-        with open(LEGACY_SCORES_FILE_LOCATION, "r"):
-            pass
-        score_file = LEGACY_SCORES_FILE_LOCATION
-    except FileNotFoundError:
-        score_file = proofs_file_location.strip(".txt") + "_scores.txt"
-        try:
-            with open(score_file, "r"):
-                pass
-        except FileNotFoundError:
-            with open(proofs_file_location, "r") as f:
-                number_of_demo = len(f.readlines())
-            with open(score_file, "w") as f:
-                f.write("1.0\n"*(number_of_demo-1) + "1.0")
-    return score_file
-
-
 def initialize_mode():
     user_input = int(input(
         "Que veux tu faire ?\n"
@@ -119,51 +124,63 @@ def initialize_mode():
     return user_input
 
 
-scores_file_location = initialize_score_file(PROOFS_FILE_LOCATION)
-mode = initialize_mode()
-while(mode == 3): # Stats mode
-    proofs_and_scores = initialize_proofs_and_scores(PROOFS_FILE_LOCATION)
-    data = get_stats()
+def stats_interface(proofs_and_scores):
+    def precise_stats(max_possible, maxs, moyennes):
+
+        for proof_number in range(len(data)):
+            print(f"\n\033[91m\033[1m" + proofs_and_scores[proof_number + 1][0] + "\033[0m")
+            print(
+                f"Elle a une moyenne de {moyennes[proof_number] == -1 and '<pas encore faite>' or moyennes[proof_number]}/{max_possible}")
+            print(f"Elle a un max de {maxs[proof_number]}")
+            print(f"Elle a été faite {len(data[proof_number])} fois")
+            print(f"Les notes sont {data[proof_number]}")
+
     moyennes = []
     maxs = []
     for i in range(len(data)):
-        if(len(data[i]) != 0):
+        if len(data[i]) != 0:
             maxs.append(max(data[i]))
         else:
             maxs.append(0)
-        if(len(data[i]) == 0):
+        if len(data[i]) == 0:
             moyennes.append(-1)
             continue
-        moyennes.append(sum(data[i])/len(data[i]))
+        moyennes.append(sum(data[i]) / len(data[i]))
 
     max_possible = max([int(el) for el in list(FACTORS_FOR_WEIGHTS_ADJUSTING.keys())])
     min_possible = min([int(el) for el in list(FACTORS_FOR_WEIGHTS_ADJUSTING.keys())])
     space = max_possible - min_possible
     liste = [m for m in moyennes if m != -1] if len([m for m in moyennes if m != -1]) != 0 else [0]
-    print(f"\nTa moyenne est de {round(sum(liste)/len(liste), 2)}/{max_possible} sur celles que tu as déjà faites")
+
+    print(
+        f"\nTa moyenne est de {round(sum(liste) / len(liste), 2)}/{max_possible} sur celles que tu as déjà faites")
     print(f"Tu as fait {len([m for m in moyennes if m != -1])} démonstrations sur {len(proofs_and_scores)}")
+
     # Create a score for everything considering only the maxs considering that 100% is when all the maxes are the max of the keys of FACTORS_FOR_WEIGHT_ADJUSTMENT
     values = list(FACTORS_FOR_WEIGHTS_ADJUSTING.values())
-    scores_per_value = [(min_possible+i)/space for i in range(len(values))]
-    score = sum([scores_per_value[maxs[i]] for i in range(len(maxs))])/len(maxs)
+    scores_per_value = [(min_possible + i) / space for i in range(len(values))]
+    score = sum([scores_per_value[maxs[i]] for i in range(len(maxs))]) / len(maxs)
+
     print(f"Ton score total est de {round(score, 2)}%\n")
-    stats_a = int(input(
+
+    continue_status = int(input(
         "Que veux tu faire ? \n"
         "   0 : Voir Des informations sur chaque démo\n"
         "   1 : Revenir en arrière\n"
         "input : "
     ))
-    if(stats_a == 1):
-        mode = initialize_mode()
-    else:
-        proofs_and_scores = initialize_proofs_and_scores(PROOFS_FILE_LOCATION)
-        for i in range(len(data)):
-            print(f"\n\033[91m\033[1m" + proofs_and_scores[i+1][0] + "\033[0m")
-            print(f"Elle a une moyenne de {moyennes[i] == -1 and '<pas encore faite>' or moyennes[i]}/{max_possible}")
-            print(f"Elle a un max de {maxs[i]}")
-            print(f"Elle a été faite {len(data[i])} fois")
-            print(f"Les notes sont {data[i]}")
-        mode = initialize_mode()
+    if continue_status == 0:
+        precise_stats(max_possible, maxs, moyennes)
+
+
+scores_file_location = initialize_score_file(PROOFS_FILE_LOCATION)
+mode = initialize_mode()
+proofs_and_scores = initialize_proofs_and_scores(PROOFS_FILE_LOCATION)
+data = get_stats()
+
+while mode == 3:
+    stats_interface(proofs_and_scores)
+    mode = initialize_mode()
 
 studying = True
 while studying:
@@ -171,7 +188,7 @@ while studying:
 
     current_proof, mode = choose_next_proof(mode, proofs_and_scores)
 
-    print("\n\033[91m\033[1m" + proofs_and_scores[current_proof][0]+ "\033[0m")
+    print("\n\033[91m\033[1m" + proofs_and_scores[current_proof][0] + "\033[0m")
     success_assessment = input(
         "Démo réussie ? \n"
         "   0 : Pas du tout\n"
@@ -181,10 +198,10 @@ while studying:
         "   4 : S'arrêter ici\n"
         "input : "
     )
-    if(success_assessment == "4"):
+    if success_assessment == "4":
         studying = False
         break
-    while(success_assessment not in FACTORS_FOR_WEIGHTS_ADJUSTING):
+    while success_assessment not in FACTORS_FOR_WEIGHTS_ADJUSTING:
         success_assessment = input("Input invalide, réessaye : ")
 
     proofs_and_scores[current_proof][1] *= FACTORS_FOR_WEIGHTS_ADJUSTING[success_assessment]
