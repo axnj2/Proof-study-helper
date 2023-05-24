2a6da840f38f99f976dc315921c13b2d2327d760
axnj2 <52408308+axnj2@users.noreply.github.com>
2023-05-24 10:21:21 +0200

    added support for multiline proofs

--- add_new_proof_to_list.py
+++ add_new_proof_to_list.py
@@ -1,14 +1,15 @@
-
 PROOF_FILE_NAME = "test_proofs.txt"
 
+
 def load_file(proof_file_name) -> dict:
-    proofs_and_scores = dict() # key: proof number, value: [proof text, score, stats, is_latex, has_answer_image]
+    proofs_and_scores = dict()  # key: proof number, value: [proof text, score, stats, is_latex, has_answer_image]
 
     with open(proof_file_name, 'r') as f:
         number_of_proofs = int(f.readline())
         for i in range(number_of_proofs):
             header = f.readline().split()
-            number_of_lines_in_proof, proof_number, is_latex, has_answer_image = int(header[0]), int(header[1]), bool(header[2]), bool(header[3])
+            number_of_lines_in_proof, proof_number, is_latex, has_answer_image = int(header[0]), int(header[1]), bool(
+                header[2]), bool(header[3])
             proof_text = r""
             for j in range(number_of_lines_in_proof):
                 proof_text += f.readline()
@@ -27,21 +28,26 @@ def write_to_proof_file(proof_file_name: str, data: dict):
             f.write(proof_text + "\n")
 
 
+def str2bool(v):
+    return v.lower() in ("yes", "true", "t", "1", "y")
+
+
 def ask_for_new_proof_question():
     # input raw string
-    proof_text = input("Enter your new proof question: ") # TODO : only works for one line at the moment needs to be fixed
-    has_image = input("Does your proof have an image? (y/n): ")
-    if has_image == "y":
-        has_image = True
-    else:
-        has_image = False
-    is_latex = input("Is your proof in latex? (y/n): ")
-    if is_latex == "y":
-        is_latex = True
-    else:
-        is_latex = False
+    proof_text = ""
+    new_proof_line = input("Enter your new proof question (return a single \"e\" to finish): \n") + "\n"
+    while new_proof_line != "e\n":
+        proof_text += new_proof_line
+        new_proof_line = input() + "\n"
+    proof_text = proof_text.strip()
+    # input metadata
+    has_image = str2bool(input("Does your proof have an image? (y/n): "))
+    is_latex = str2bool(input("Is your proof in latex? (y/n): "))
+
     new_proof = [proof_text, None, None, is_latex, has_image]
+
     proof_number = int(input("Enter the proof number: "))
+
     return proof_number, new_proof
 
 
@@ -53,8 +59,4 @@ if __name__ == "__main__":
         proofs_and_scores[proof_number] = new_proof
         write_to_proof_file(PROOF_FILE_NAME, proofs_and_scores)
 
-        working = input("Do you want to add another proof? (y/n): ")
-        if working == "y":
-            working = True
-        else:
-            working = False
+        working = str2bool(input("Do you want to add another proof? (y/n): "))
